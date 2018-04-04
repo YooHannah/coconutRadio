47fc995e2d0f12d8fae514eedf1c1d7feadcbed6
unknown <liuyh001@cmrh.com>
2018-04-04 18:01:19 +0800

    111

--- pages/newest/newest.js
+++ pages/newest/newest.js
@@ -2,40 +2,48 @@
 const app = getApp();
 Page({
   data: {
-  codeimg:'../../img/radio.jpg',
-  imgalist: ['http://www.3987.com/uploadfile/2017/0415/20170415105145627.png']
+    playShow: true,
+    pauseShow: false,
+    info:{}
   },
   onLoad: function (options) {
     let self = this;
+    self.audioCtx = wx.createAudioContext('myAudio')
     wx.getStorage({
       key: 'oneItem',
       success: function (res) {
         console.log(res.data)
-        if(res.data.id){ //一般试听
+        if (res.data.id) { //一般试听
           self.setData({
-            codeimg: res.data.img
+            info: res.data
           })
-        }else{
+        } else {
           //去获取最新一期
-          // app.func.http(app.globalData.interface.getnewest, 'GET',     app.globalData.userInfo, {})
-          //   .then(function (response) {
-          //     // that.setData({
-          //     //   positionlist: response.data.list
-          //     // });
-          //     console.log(response);
-          //   }, function (error) {
-          //     console.log(error);
-          //   });
+          app.func.http(app.globalData.interface.getnewest, 'GET', app.globalData.userInfo, {})
+            .then(function (response) {
+              self.setData({
+                info: response.data
+              });
+              console.log(response);
+            }, function (error) {
+              console.log(error);
+            });
         }
       }
     })
   },
-
-// https://m.ximalaya.com/tracks/76840905.json
-  bindViewTap:function(){
-    wx.previewImage({
-      current: '',
-      urls: this.data.imgalist
-    }) 
+  audioPlay: function () {
+    this.audioCtx.play()
+    this.setData({
+      playShow: false,
+      pauseShow: true
+    })
+  },
+  audioPause: function () {
+    this.audioCtx.pause();
+    this.setData({
+      playShow: true,
+      pauseShow: false
+    })
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,11 @@
 // pages/search/search.js
+const app = getApp();
 Page({
   data: {
     keywords: '',
     results: [{
       title: '孙燕姿',
-      id: 0,
+      id: 1,
       songs: [{
         name: '当冬夜渐暖',
         poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
@@ -17,16 +18,39 @@ Page({
         src: 'http://audio.xmcdn.com/group40/M02/C1/D8/wKgJT1qrLqWS1_gWABk7781rNK8359.m4a'
       }]
     }, {
-      title: 'F.I.R',
-      id: 1,
-      songs: [{
-        title: '你的微笑'
-      }, {
-        title: '雨樱花'
-      }]
+      "id": 76840905,
+      "play_path_64": "http://audio.xmcdn.com/group40/M02/C1/D8/wKgJT1qrLqWS1_gWABk7781rNK8359.m4a",
+      "play_path_32": "http://audio.xmcdn.com/group40/M02/C1/D8/wKgJT1qrLqSDsKIaAAmoSy5KokY407.m4a",
+      "play_path": "http://audio.xmcdn.com/group40/M02/C1/D8/wKgJT1qrLqWS1_gWABk7781rNK8359.m4a",
+      "duration": 204,
+      "title": "Rainy Mood",
+      "nickname": "3D体验馆",
+      "uid": 1000139,
+      "waveform": "group40/M02/C2/CD/wKgJVFqrLqWBAMJsAAAJ8zMYL4U8574.js",
+      "upload_id": "u_79999868",
+      "cover_url": "http://fdfs.xmcdn.com/group30/M02/F7/B6/wKgJXlmIFT7yp4oKAABi2-DeRVI855.jpg",
+      "cover_url_142": "http://fdfs.xmcdn.com/group30/M02/F7/B6/wKgJXlmIFT7yp4oKAABi2-DeRVI855_web_large.jpg",
+      "formatted_created_at": "3月16日 10:41",
+      "is_favorited": false,
+      "play_count": 14230,
+      "comments_count": 0,
+      "shares_count": 0,
+      "favorites_count": 9,
+      "album_id": 9843091,
+      "album_title": "助眠圣器，安静舒缓的3D音乐",
+      "intro": "",
+      "have_more_intro": false,
+      "time_until_now": "19天前",
+      "category_name": "music",
+      "category_title": "音乐",
+      "played_secs": null,
+      "is_paid": false,
+      "is_free": null,
+      "price": null,
+      "discounted_price": null
     }, {
       title: 'S.H.E',
-      id: 2,
+      id: 3,
       songs: [{
         title: 'super star'
       }, {
@@ -41,6 +65,17 @@ Page({
   },
   bindSearch: function () {
     console.log(this.data.keywords);
+    let self = this;
+    let senddata = { keywords: this.data.keywords, userInfo: app.globalData.userInfo}
+    app.func.http(app.globalData.interface.search, 'GET',senddata, {})
+      .then(function (response) {
+        self.setData({
+          result: response.data
+        });
+        console.log(response);
+      }, function (error) {
+        console.log(error);
+      });
   },
   bindJumpDetail: function (e) {
     // console.log(e.currentTarget.dataset.val)
